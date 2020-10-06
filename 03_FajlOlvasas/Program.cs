c65eef2e03a420721e8468d0c0c5ca369ae85c16
Sensei <toth.tanarur@pm.me>
2020-10-06 08:29:18 +0200

    proba

---
+++ 03_FajlOlvasas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _03_FajlOlvasas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fajl = "teszt.txt";
+            StreamReader sr = new StreamReader(fajl);
+
+            string elso = sr.ReadLine();
+            string masodik = sr.ReadLine();
+
+            sr.Close();
+
+            string[] sor;
+            sor = elso.Split(',');
+
+            Console.WriteLine(elso);
+            Console.WriteLine(masodik);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
