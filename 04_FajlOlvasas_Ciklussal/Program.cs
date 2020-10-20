1a35879a714b7dfc979c4417570ba1614f7e6d26
Sensei <toth.tanarur@pm.me>
2020-10-20 09:11:43 +0200

    Fájl írása ciklussal

--- 04_FajlOlvasas_Ciklussal/Program.cs
+++ 04_FajlOlvasas_Ciklussal/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _04_FajlOlvasas_Ciklussal
 {
@@ -10,7 +11,45 @@ namespace _04_FajlOlvasas_Ciklussal
     {
         static void Main(string[] args)
         {
+            string file = "services.txt";
+            StreamReader sr = new StreamReader(file);
+            int sorokszama = 0;
+            string[] sortomb = new string[300];
+            int index = 0;
 
+            #region Beolvasás
+            while (sr.EndOfStream == false)
+            {   
+                string sor = sr.ReadLine();
+                sortomb[index] = sor;                
+                index++;
+                sorokszama++;
+
+                Console.WriteLine(sor);
+
+                if (sorokszama == 28)
+                {
+                    sorokszama = 0;
+                    Console.Write("Üss egy ENTER-t a folytatáshoz!");
+                    Console.ReadLine();
+                }
+            }
+
+            sr.Close();
+            #endregion
+
+            int tcpdb = 0;
+            string elem;
+            for (int i = 0; i < index; i++)
+            {
+                elem = sortomb[i];
+                if (elem.Contains("tcp") == true)
+                    tcpdb++;
+            }
+
+            Console.WriteLine("\nAz állományban {0} sor van!",index);
+            Console.WriteLine("Az állományban {0} db tcp kifejezés fordul elő!",tcpdb);
+            Console.ReadLine();
 
         }
     }
---
+++ 05_FajlIrasa_Ciklussal/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _05_FajlIrasa_Ciklussal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generáljunk le 100 db háromjegyű véletlenszámot, majd azokat vesszővel elválasztva
+            //írjuk ki egy "velszam.txt" nevű állományba vesszőkkel elválasztva, úgy
+            //hogy egy sorba 10 db szám kerüljön.
+
+            int[] szamok = new int[100];
+            Random rnd = new Random();
+
+            string file = "velszam.txt";
+            StreamWriter sw = new StreamWriter(file);
+
+            string sor = "";
+            for(int i = 1;i<=100;i++)
+                {
+                szamok[i-1] = rnd.Next(100, 1000);
+
+
+                #region sor létrehozása 1. változat
+                /*
+                 //Ha 9 elemet már a sorba tettünk, akkor a tizediket
+                 //már vessző nélkül rakjuk a végére, majd kiírjuk a 
+                 //képernyőre és a fájlba. Végül "lenullázuk" a sorunkat!
+                if (i % 10 == 0)
+                {                    
+                    sor = sor + szamok[i - 1];
+                    Console.WriteLine(sor);
+                    sor = "";
+                }
+                else
+                //Ha még nem generáltunk le 10 elemet, akkor hozzáadjuk a sorhoz
+                //vesszővel
+                {
+                    sor = sor + szamok[i - 1] + ",";
+                }
+                */
+                #endregion
+
+
+                #region sor létrehozása 2. változat
+
+                //Felépítjük a sor, mindig hozzáadjuk a következő elemet 
+                //ÉS egy vesszőt is. Azért i-1 az index, mert a ciklusunk
+                //1-től indult, hogy egyszerűbben tudjuk figyelni, ha 
+                //10 számot már legeneráltunk!
+                sor = sor + szamok[i - 1] + ",";
+
+                if (i % 10 == 0)
+                {
+                    //Ha 10 elemet már a sorba tettünk, akkor levágjuk
+                    //a végéről a vesszőt, majd kiírjuk a képernyőre és a 
+                    //fájlba. Végül "lenullázuk" a sorunkat!
+                    sor = sor.TrimEnd(',');
+                    Console.WriteLine(sor);
+                    sw.WriteLine(sor);
+                    sor = "";
+                }
+                #endregion
+
+
+            }
+
+            sw.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
